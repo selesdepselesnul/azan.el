18f74f629f19cd6e6d13c891382f8b567993f0f0
selesdepselesnul <selesdepselesnul@gmail.com>
2017-10-13 21:33:52 +0700

    add handler

--- adzan.el
+++ adzan.el
@@ -1,12 +1,22 @@
-(defvar isya "20:24")
-
 (defun get-time-hh-mm ()
   (let* ((splitted-time (split-string (format-time-string "%T") ":"))  
 	 (hour (nth 0 splitted-time))
 	 (minute (nth 1 splitted-time)))
     (concat hour ":" minute)))
 
-(string= "20:43" (get-time))
+(defun display-adzan (time)
+  (progn
+    (switch-to-buffer time)
+    (insert time)))
 
+(defun adzan-handler ()
+  (let* ((time-now (get-time-hh-mm)))
+    (cond ((string= "04:43" time-now) (message "subuh")
+	   (string= "12:00" time-now) (message "dzuhur")
+	   (string= "15:43" time-now) (message "ashar")
+	   (string= "17:43" time-now) (message "maghrib")
+	   (string= "21:33" time-now) (message "isya")))))
+
+(run-at-time 0 1 'adzan-handler)
 
 
