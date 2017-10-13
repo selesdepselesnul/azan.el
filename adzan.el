804435a4d878203e080158c12eda2970ee172589
selesdepselesnul <selesdepselesnul@gmail.com>
2017-10-14 05:15:29 +0700

    fix the cond

--- adzan.el
+++ adzan.el
@@ -6,17 +6,20 @@
 
 (defun display-adzan (time)
   (progn
+    (cancel-timer timer)
     (switch-to-buffer time)
     (insert time)))
 
 (defun adzan-handler ()
   (let* ((time-now (get-time-hh-mm)))
-    (cond ((string= "04:43" time-now) (message "subuh")
-	   (string= "12:00" time-now) (message "dzuhur")
-	   (string= "15:43" time-now) (message "ashar")
-	   (string= "17:43" time-now) (message "maghrib")
-	   (string= "21:33" time-now) (message "isya")))))
+    (cond
+     ((string= "05:15" time-now) (display-adzan "subuh"))
+     ((string= "12:00" time-now) (display-adzan "dzuhur"))
+     ((string= "15:43" time-now) (display-adzan "ashar"))
+     ((string= "17:43" time-now) (display-adzan "maghrib"))
+     ((string= "21:33" time-now) (display-adzan "isya")))))
 
-(run-at-time 0 1 'adzan-handler)
+(defvar timer (run-at-time 0 1 'adzan-handler))
+
 
 
